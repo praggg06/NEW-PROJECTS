4481c74ef91ada20dccaacd23df5ee12aad7c147
shreya-j-anand <shreyajanand@gmail.com>
2026-06-21 00:00:17 +0530

    Add files via upload

---
+++ src/data/mockBookings.js
@@ -0,0 +1,10 @@
+export const mockBookings = [
+  {
+    id: 1,
+    consumer: "Pragati",
+    state: "Himachal Pradesh",
+    district: "Shimla",
+    distributor: "HP Gas Shimla",
+    status: "Pending Approval",
+  },
+];
